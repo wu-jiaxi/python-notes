48e253c3d30925a31abbb685b0ffd2bddb9eaaa2
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2024-01-27 00:02:03 -0600

    uses regex to sarch for good/not user and count them

--- cheatSheet.py
+++ cheatSheet.py
@@ -192,3 +192,24 @@ while(cont.lower() == "y"):
     
 print("Goodbye!")
 
+
+#uses regex to search for good/naughty user and count them
+
+import re
+import sys
+
+logfile = sys.argv[1]
+usernames = {}
+with open(logfile) as f:
+  for line in f:
+    if "CRON" not in line:
+      continue
+    pattern = r"USER \((\w+)\)$"
+    result = re.search(pattern, line)
+
+    if result is None:
+      continue
+    name = result[1]
+    usernames[name] = usernames.get(name, 0) + 1
+
+print(usernames)
